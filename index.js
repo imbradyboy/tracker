64e57cb29124762bb9c10ea8993129a3c77af3b5
digimbyte <digimbyte@gmail.com>
2021-07-20 09:14:16 +1000

    basic exports

---
+++ index.js
@@ -0,0 +1,12 @@
+exports.realtime = function (snapshot, tag) {
+    return new promise((resolve, reject) => { })
+}
+exports.firestore = function (snapshot, tag) {
+    return new promise((resolve, reject) => { })
+}
+exports.storage = function (snapshot, tag) {
+    return new promise((resolve, reject) => { })
+}
+exports.meta = function (snapshot, tag) {
+    return new promise((resolve, reject) => { })
+}
