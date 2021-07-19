a611d155181378a688621409a2dc9cb9aacbcc6d
digimbyte <digimbyte@gmail.com>
2021-07-20 09:19:42 +1000

    notes

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+// Need to import Firebase, access current auth
+// Log Auth UID
+// Log the reference path
+// Log the size of the payload
+// Log the custom tag with it.
+
 exports.realtime = function (snapshot, tag) {
-    return new promise((resolve, reject) => { })
+    return new promise((resolve, reject) => {
+     })
 }
 exports.firestore = function (snapshot, tag) {
     return new promise((resolve, reject) => { })
